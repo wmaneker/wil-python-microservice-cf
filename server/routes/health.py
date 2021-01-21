19ec848fc7c74ae9dc655fe4c3484cc54973cdb0
wmaneker <wmaneker@yahoo.com>
2021-01-21 10:43:00 -0500

    Created '/answer/<number>' endpoint

--- server/routes/health.py
+++ server/routes/health.py
@@ -7,3 +7,8 @@ def health():
     """health route"""
     state = {"status": "UP"}
     return jsonify(state)
+
+@app.route('/answer')
+def answer():
+    answer = {'The Answer to Life the Universe and Everything': 42}
+    return jsonify(answer)
