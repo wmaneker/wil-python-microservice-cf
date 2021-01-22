129f83665bb2a42672985977a066e7e6edc5718a
wmaneker <wmaneker@yahoo.com>
2021-01-21 19:39:31 -0500

    updated pipfile and generated new piplock

--- server/__init__.py
+++ server/__init__.py
@@ -1,8 +1,10 @@
 import os
 from flask import Flask, abort, session, request, redirect
 from flask.json import jsonify
+from flask_restx import Api
 
 app = Flask(__name__, template_folder="../public", static_folder="../public", static_url_path='')
+api = Api(app, title='My first Python API', version='1.0', doc='/apidocs/', description='A number-crunching API')
 
 from server.routes import *
 from server.services import *
